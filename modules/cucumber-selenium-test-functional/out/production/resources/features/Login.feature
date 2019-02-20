bb7b6984c8aad740c3af2149c07040fa9d0df6b1
Shane Merriss <shane.merriss@liferay.com>
2019-02-20 11:39:38 -0800

    Use fluentlenium

--- modules/cucumber-selenium-test-functional/out/production/resources/features/Login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Test the login functionality of Jarvis site
-
-  Background:
-	Given I go to the login page
-
-  Scenario: Correct username and password
-	Given I log in as 'test@liferay.com' with password 'test'
--- modules/cucumber-selenium-test-functional/src/main/java/com/liferay/cucumber/selenium/test/functional/step/BaseSteps.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.cucumber.selenium.test.functional.step;
-
-import com.liferay.cucumber.selenium.test.functional.BaseWebDriver;
-
-/**
- * @author Shane Merriss
- */
-public class BaseSteps extends BaseWebDriver {
-}
